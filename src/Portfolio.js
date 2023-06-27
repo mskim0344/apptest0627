dfaec01539cd095537c5b8f01f2333d89426eb15
mskim0344 <mskim0344@gmail.com>
2023-06-27 12:48:38 +0900

    플러그인 부착완료 ~`

---
+++ src/Portfolio.js
@@ -0,0 +1,22 @@
+const Portfolio = () =>{
+    return(
+        <>
+            <header id="hd">
+
+            </header>
+            <div id="slide">
+            </div>
+            <div id="review">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-4">1</div>
+                        <div className="col-4">2</div>
+                        <div className="col-4">3</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Portfolio
